e450bc513884df29d20b621c472a8e5ad376a795
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-11-03 17:18:04 +0100

    aggiunto css a FilmPage

--- pages/FilmPage.jsx
+++ pages/FilmPage.jsx
@@ -1,10 +1,25 @@
+import './FilmPage.css'
+
 export default function FilmPage() {
-    return(
+    return (
         <>
-        <h1>Nome Film</h1>
-        <div>
-            <p>Recensioni film</p>
-        </div>
+            <div className="bg-pages">
+                <h1 class="film-title">Nome Film</h1>
+
+                <div class="reviews-list">
+                    <div class="review-card">
+                        <div class="review-content">
+                            <p>"Un film bellissimo, mi ha emozionato dall'inizio alla fine!"</p>
+                        </div>
+                    </div>
+
+                    <div class="review-card">
+                        <div class="review-content">
+                            <p>"Buona regia ma finale un po' debole."</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
