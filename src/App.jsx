1ed3776183d702e11ce5e7c6ab2d0894046dcfb3
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-11-03 16:06:44 +0100

    aggiunte page alle rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+// importo elementi per gestione router
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// importo il layout
+import DefaultLayout from "../layouts/DefaultLayout"
+
+// importo le page
+import HomePage from "../pages/HomePage"
+import FilmPage from "../pages/FilmPage"
 
 
 function App() {
- 
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/films/:id' element={<FilmPage/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
