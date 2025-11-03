55fc69ac2b3a33071997c29c9dba6833cb6a2a19
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-11-03 16:37:22 +0100

    aggiunto css a HomePage

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,10 +1,21 @@
+import './HomePage.css'
+
 export default function HomePage() {
-    return(
+    return (
         <>
-        <h1>Lista Film</h1>
-        <div>
-            <p>Card film</p>
-        </div>
+            <h1 class="page-title">Lista Film</h1>
+
+            <div class="film-list">
+                <div class="film-card">
+                    <p>Titolo del Film</p>
+                </div>
+                <div class="film-card">
+                    <p>Altro Film</p>
+                </div>
+                <div class="film-card">
+                    <p>Ancora un Film</p>
+                </div>
+            </div>
         </>
     )
 }
