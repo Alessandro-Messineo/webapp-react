bff276baacec5af2433137aab0fa68ea26057eca
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-11-03 17:18:35 +0100

    migliorie generali al css

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -3,17 +3,19 @@ import './HomePage.css'
 export default function HomePage() {
     return (
         <>
-            <h1 class="page-title">Lista Film</h1>
+            <div className="bg-pages">
+                <h1 class="page-title">Lista Film</h1>
 
-            <div class="film-list">
-                <div class="film-card">
-                    <p>Titolo del Film</p>
-                </div>
-                <div class="film-card">
-                    <p>Altro Film</p>
-                </div>
-                <div class="film-card">
-                    <p>Ancora un Film</p>
+                <div class="film-list">
+                    <div class="film-card">
+                        <p>Titolo del Film</p>
+                    </div>
+                    <div class="film-card">
+                        <p>Altro Film</p>
+                    </div>
+                    <div class="film-card">
+                        <p>Ancora un Film</p>
+                    </div>
                 </div>
             </div>
         </>
