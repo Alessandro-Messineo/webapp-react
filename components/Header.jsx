e93d5cb24f0c2c4f979a3f4d8c8a21c2bf0862e1
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-11-03 15:48:09 +0100

    aggiunto componente Header

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+// importo link da router-dom 
+import { Link } from "react-router-dom"
+
+
+export default function Header() {
+    return (
+        <nav >
+            <div>
+                <Link to="/">Bool Films</Link>
+            </div>
+        </nav>
+    )
+}
+
