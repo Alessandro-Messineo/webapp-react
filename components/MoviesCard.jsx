85109903eb5e64f80e29c20993b8f29cacde6ddd
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-11-04 15:43:36 +0100

    cambiato filmCard con MoviesCard

---
+++ components/MoviesCard.jsx
@@ -0,0 +1,9 @@
+export default function MoviesCard() {
+    return (
+        <>
+            <div class="film-card">
+                <p>Titolo del Film</p>
+            </div>
+        </>
+    )
+}
