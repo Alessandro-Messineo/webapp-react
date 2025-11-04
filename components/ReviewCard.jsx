245ffdb4bc5a110b5b2bd0f8f790c8f2dd071f0a
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-11-04 16:09:10 +0100

    creato componente ReviewCard

---
+++ components/ReviewCard.jsx
@@ -0,0 +1,14 @@
+export default function ReviewCard() {
+    return (
+        <>
+        <div>
+            
+        </div>
+            <div class="review-card">
+                <div class="review-content">
+                    <p>"Un film bellissimo, mi ha emozionato dall'inizio alla fine!"</p>
+                </div>
+            </div>
+        </>
+    )
+}
