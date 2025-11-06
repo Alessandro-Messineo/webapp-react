055f4737c591d27e534c3664ae56f2f04873c37b
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-11-06 16:15:53 +0100

    importato il provider

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,23 @@ import DefaultLayout from "../layouts/DefaultLayout"
 import HomePage from "../pages/HomePage"
 import FilmPage from "../pages/FilmPage"
 
+// importo del provider context globale
+import { GlobalProvider } from "../contexts/GlobalContext"
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path='/' element={<HomePage/>}/>
-            <Route path='/movies/:id' element={<FilmPage/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <GlobalProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/movies/:id' element={<FilmPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalProvider>
     </>
   )
 }
