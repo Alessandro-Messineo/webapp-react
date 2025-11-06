a3654aaf161c9dade6ba82a68dc9d8b4feb35585
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-11-06 16:11:45 +0100

    renderizzazione componente nel layout

--- layouts/DefaultLayout.jsx
+++ layouts/DefaultLayout.jsx
@@ -4,14 +4,21 @@ import { Outlet } from "react-router-dom"
 // importo Header
 import Header from "../components/Header"
 
+// import hook per contesto
+import { useGlobal } from "../contexts/GlobalContext" 
+
 
 export default function DefaultLayout() {
+
+     const { isLoading } = useGlobal();
+
     return (
         <>
             <Header />
             <main>
                 <Outlet />
             </main>
+            {isLoading && <Loader />}
         </>
     )
 }
